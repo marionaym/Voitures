697bd22178dc87c11e251f33e764a9b4fc431bd9
marionymd <marionn.aymm@gmail.com>
2021-11-23 08:50:46 +0100

    devoir fini

--- src/main/java/garages/Stationnement.java
+++ src/main/java/garages/Stationnement.java
@@ -5,48 +5,48 @@ import java.util.Date;
 
 public class Stationnement {
 
-	private final Voiture myCar;
-	private final Garage myGarage;
+    private final Voiture myCar;
+    private final Garage myGarage;
 
-	private final Date entree = new Date(); // Aujourd'hui
-	private Date fin;
+    private final Date entree = new Date(); // Aujourd'hui
+    private Date fin;
 
-	public Stationnement(Voiture v, Garage g) {
-		myCar = v;
-		myGarage = g;
-	}
+    public Stationnement(Voiture v, Garage g) {
+        myCar = v;
+        myGarage = g;
+    }
 
-	public Voiture getCar() {
-		return myCar;
-	}
+    public Voiture getCar() {
+        return myCar;
+    }
 
-	public Garage getGarage() {
-		return myGarage;
-	}
+    public Garage getGarage() {
+        return myGarage;
+    }
 
-	public Date getEntree() {
-		return entree;
-	}
+    public Date getEntree() {
+        return entree;
+    }
 
-	public Date getFin() {
-		return fin;
-	}
+    public Date getFin() {
+        return fin;
+    }
 
-	public void terminer() {
-		fin = new Date();
-	}
+    public void terminer() {
+        fin = new Date();
+    }
 
-	public boolean estEnCours() {
-		return (fin == null);
-	}
+    public boolean estEnCours() {
+        return (fin == null);
+    }
 
-	@Override
-	public String toString() {
-		SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyy");
-		return String.format("Stationnement{ entree=%s, %s }",
-			dateFormat.format(entree),
-			estEnCours() ? "en cours" : "sortie=" + dateFormat.format(fin)
-		);
-	}
+    @Override
+    public String toString() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyy");
+        return String.format("Stationnement{ entree=%s, %s }",
+                dateFormat.format(entree),
+                estEnCours() ? "en cours" : "sortie=" + dateFormat.format(fin)
+        );
+    }
 
 }
